2816dff41a6dba671a82ddbcbe727ff7b00a8d12
Ephraim Djeket <silentcodedaily@gmail.com>
2026-03-22 19:53:54 +0100

    Finished project

--- workout-hero-challenge/main.js
+++ workout-hero-challenge/main.js
@@ -1,64 +1,34 @@
-const hamburgerMenu = document.getElementById("hamburger-menu-btn");
 const hamburgerBtn = document.getElementById("hamburger-btn");
+const menu = document.getElementById("menu");
 const exitBtn = document.getElementById("exit-btn");
-const ulMenu = document.getElementById("menu");
-const logo = document.getElementById("logo");
-const firstAside = document.getElementById("first-aside");
-const secondAside = document.getElementById("second-aside");
 const main = document.getElementById("main");
 
 function hamburgerBtnF() {
-  hamburgerBtn.classList.add("hidden");
   exitBtn.classList.remove("hidden");
-  hamburgerMenu.classList.add("w-full");
-  hamburgerMenu.classList.add("px-16");
-  ulMenu.classList.toggle("hidden");
-  logo.classList.toggle("hidden");
-  firstAside.classList.add("hidden");
-  secondAside.classList.add("hidden");
+  hamburgerBtn.classList.add("hidden");
+  menu.classList.remove("hidden");
   main.classList.add("hidden");
 }
 
 function exitBtnF() {
-  hamburgerBtn.classList.remove("hidden");
   exitBtn.classList.add("hidden");
-  hamburgerMenu.classList.remove("w-full");
-  hamburgerMenu.classList.remove("px-16");
-  ulMenu.classList.toggle("hidden");
-  logo.classList.toggle("hidden");
-  firstAside.classList.toggle("hidden");
-  secondAside.classList.toggle("hidden");
-  main.classList.toggle("hidden");
+  hamburgerBtn.classList.remove("hidden");
+  menu.classList.add("hidden");
+  main.classList.remove("hidden");
 }
-
-hamburgerBtn.addEventListener("click", hamburgerBtnF);
-exitBtn.addEventListener("click", exitBtnF);
 
 const breakpoint = window.matchMedia("(max-width: 640px)");
 
-function handleBreakpointChange(e) {
+function handleBreakPointChange(e) {
   if (!e.matches) {
-    hamburgerBtn.classList.remove("hidden");
     exitBtn.classList.add("hidden");
-    hamburgerMenu.classList.remove("w-full");
-    hamburgerMenu.classList.remove("px-16");
-    ulMenu.classList.add("hidden");
-    logo.classList.toggle("hidden");
-    firstAside.classList.add("hidden");
-    secondAside.classList.add("hidden");
-    main.classList.add("hidden");
-  } else {
     hamburgerBtn.classList.remove("hidden");
-    exitBtn.classList.add("hidden");
-    hamburgerMenu.classList.remove("w-full");
-    hamburgerMenu.classList.remove("px-16");
-    ulMenu.classList.add("hidden");
-    logo.classList.add("hidden");
-    firstAside.classList.add("hidden");
-    secondAside.classList.add("hidden");
+    menu.classList.add("hidden");
     main.classList.remove("hidden");
   }
 }
 
-breakpoint.addEventListener("change", handleBreakpointChange);
-handleBreakpointChange(breakpoint);
+breakpoint.addEventListener("change", handleBreakPointChange);
+hamburgerBtn.addEventListener("click", hamburgerBtnF);
+exitBtn.addEventListener("click", exitBtnF);
+handleBreakPointChange(breakpoint);
